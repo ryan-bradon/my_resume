8de286ef2f446c56f71679a094bb1e73a6726870
Ryan Bradon <7089274+ryan-bradon@users.noreply.github>
2024-09-20 13:09:54 -0700

    changing to production env vars for digitial ocean

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,6 +1,32 @@
 from .base import *
+import os
+from urllib.parse import urlparse
+
 
 DEBUG = False
+DATABASE_URL = os.getenv('DATABASE_URL', None)
+ALLOWED_HOSTS = ["*"]
+
+if not DATABASE_URL:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    db_info = urlparse(DATABASE_URL)
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'db',
+            'USER': db_info.username,
+            'PASSWORD': db_info.password,
+            'HOST': db_info.hostname,
+            'PORT': db_info.port,
+            'OPTIONS': {'sslmode': 'require'},
+        }
+    }
 
 try:
     from .local import *
