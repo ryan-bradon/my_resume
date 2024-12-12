6767ab52cb1738ff81419ccbf94e458ca4251c44
Ryan Bradon <ryan.bradon@gmail.com>
2024-12-12 11:47:37 -0800

    project init

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-&9d^mw2-@_(m@+3=&90xl&mhx4^=iv0yaf6o@yi8$-6e*l(1q4"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
